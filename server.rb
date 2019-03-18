c35bc04675d68756d75247957de5870a88f18942
Brianna Kincart <briannakincart@gmail.com>
2019-03-18 14:39:12 -0400

    update require_relative

--- server.rb
+++ server.rb
@@ -3,6 +3,6 @@ require 'sinatra/reloader'
 
 require 'pry'
 require 'csv'
-require_relative 'app/restaurant.rb'
+require_relative 'app/models/restaurant.rb'
 
 set :bind,'0.0.0.0'  # bind to all interfaces, http://www.sinatrarb.com/configuration.html
