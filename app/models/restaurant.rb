3be3928d79fb3c1c63d22d9f075b51c5e4b4dce3
Stephen Fang <safang99@gmail.com>
2020-03-16 17:13:44 -0400

    final 28 code

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant
-
+  attr_reader :name, :address, :description, :url, :image, :id
+  def initialize(id, name, address, description, url, image)
+    @id = id
+    @name = name
+    @address = address
+    @description = description
+    @url = url
+    @image = image
+  end
 end
--- server.rb
+++ server.rb
@@ -6,3 +6,66 @@ require 'csv'
 require_relative 'app/models/restaurant.rb'
 
 set :bind,'0.0.0.0'  # bind to all interfaces, http://www.sinatrarb.com/configuration.html
+
+def get_restaurants
+  restaurants = []
+
+  CSV.foreach('restaurants.csv', headers: true) do |row|
+
+    restaurants << Restaurant.new(
+      row["id"],
+      row["name"],
+      row["address"],
+      row["description"],
+      row["url"],
+      row["image"]
+    )
+  end
+  restaurants
+end
+
+get '/restaurants' do
+  @restaurants = get_restaurants
+  erb :index
+end
+
+get "/restaurants/new" do
+  erb :new
+end
+
+get '/restaurants/:id' do
+
+  CSV.foreach('restaurants.csv', headers: true) do |row|
+
+    if params["id"] == row["id"]
+      @restaurant = Restaurant.new(
+        row["id"],
+        row["name"],
+        row["address"],
+        row["description"],
+        row["url"],
+        row["image"]
+      )
+    end
+  end
+  erb :show
+end
+
+post '/restaurants' do
+  @restaurants = get_restaurants
+  id = @restaurants.last.id.to_i + 1
+
+    CSV.open('restaurants.csv', 'a') do |csv|
+      csv << [
+        id,
+        params["name"],
+        params["address"],
+        params["description"],
+        params["url"],
+        params["image"]
+      ]
+    end
+
+# redirect "/restaurants"
+redirect "/restaurants/#{id}"
+end
