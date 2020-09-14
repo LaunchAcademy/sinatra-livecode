943fd488474919e356c07e561f4720c6c5f209ac
nwalberts <nwalberts5@gmail.com>
2020-09-14 17:50:45 -0400

    done

--- server.rb
+++ server.rb
@@ -3,6 +3,41 @@ require 'sinatra/reloader'
 
 require 'pry'
 require 'csv'
-require_relative 'app/models/restaurant.rb'
+# require_relative 'app/models/restaurant.rb'
 
 set :bind,'0.0.0.0'  # bind to all interfaces, http://www.sinatrarb.com/configuration.html
+
+get "/restaurants" do 
+  @restaurants = CSV.readlines("restaurants.csv", headers: true)
+
+  erb :index
+end
+
+post "/restaurants" do 
+  new_name = params[:restaurant_name]
+
+  CSV.open("restaurants.csv", "a") do |csv|
+    csv << [new_name]
+  end 
+
+  redirect "/restaurants"
+end
+
+get "/restaurants/new" do 
+  erb :new
+end
+
+get "/restaurants/:id" do 
+  restaurant_id = params[:id]
+
+  @restaurants_array = CSV.readlines("restaurants.csv", headers: true)
+
+  @restaurants_array.each do |restaurant_hash|
+    if restaurant_hash["id"] == restaurant_id
+      @restaurant = restaurant_hash
+    end
+  end
+
+  erb :show
+end
+
